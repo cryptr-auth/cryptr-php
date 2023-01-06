811d971a76392a64d0f1aa278ef8a87003cea378
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-01-06 19:43:05 +0100

    init

---
+++ src/Cryptr/CryptrClaimsValidator.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Cryptr;
+
+use DateTime;
+use Exception;
+
+class CryptrClaimsValidator
+{
+  public function __construct(protected string $issuer, protected array $allowedOrigins)
+  {
+    
+  }
+  
+  public function isValid(object $decodedToken)
+  {
+    return $this->validateExpiration($decodedToken) &&
+      $this->validateIssuedAt($decodedToken)
+      && $this->validateIssuer($decodedToken)
+      && $this->validateAudience($decodedToken);
+  }
+
+  public function validateExpiration(object $decodedToken): bool
+  {
+    $expiration = DateTime::createFromFormat('U', $decodedToken->exp);
+    if ($expiration < $this->currentTime()) {
+      throw new Exception('The expiration of the JWT claim (exp) should be greater than current time');
+    }
+
+    return true;
+  }
+
+  public function validateIssuedAt(object $decodedToken): bool
+  {
+    $issuedAt = DateTime::createFromFormat('U', $decodedToken->iat);
+    if ($this->currentTime() < $issuedAt) {
+      throw new Exception('The issuedAt of the JWT claim (iat) should be lower than current time');
+    }
+    return true;
+  }
+
+  public function validateIssuer(object $decodedToken): bool
+  {
+    if ($decodedToken->iss != $this->issuer) {
+      throw new Exception('The issuer of the JWT claim (iss) must conform to the issuer from config');
+    }
+    return true;
+  }
+
+  public function validateAudience(object $decodedToken): bool
+  {
+    if(!in_array($decodedToken->aud, $this->allowedOrigins)) {
+      throw new Exception('The audience of the JWT claim (aud) must conform to audience from config');
+    }
+    return true;
+  }
+
+  public static function validateResourceOwner(object $decodedToken, string $userId): bool
+  {
+    if ($decodedToken->sub != $userId) {
+      throw new Exception('The resource owner identifier (cryptr user id) of the JWT claim (sub) is not compliant');
+    }
+
+    return true;
+  }
+
+  public static function validateScopes(object $decodedToken, array $authorizedScopes): bool
+  {
+    if (array_intersect($decodedToken->scp, $authorizedScopes) != $decodedToken->scp) {
+      throw new Exception('The scopes of the JWT claim (scp) are not compliants');
+    }
+
+    return true;
+  }
+
+
+  protected function currentTime(): DateTime
+  {
+    return new DateTime();
+  }
+}
---
+++ tests/Cryptr/CryptrClaimsValidatorTest.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once __DIR__ . '../../../vendor/autoload.php';
+
+use Cryptr\CryptrClaimsValidator;
+use PHPUnit\Framework\TestCase;
+
+class CryptrClaimsValidatorTest extends TestCase
+{
+  public function testValidRightIssuer()
+  {
+    $issuer = 'http://localhost:4000/t/shark-academy';
+    $validator = new CryptrClaimsValidator($issuer, []);
+    $decodedToken = (object) ['iss' => $issuer];
+    $this->assertTrue($validator->validateIssuer($decodedToken));
+  }
+  
+  public function testValidRightAudience()
+  {
+    $audience = 'http://localhost:3000';
+    $validator = new CryptrClaimsValidator("", [$audience]);
+    $decodedToken = (object) ['aud' => $audience];
+    $this->assertTrue($validator->validateAudience($decodedToken));
+  }
+
+  public function testRightResourceOwner()
+  {
+    $resourceOwnerId = '75ee9050-453a-475f-8528-1f92b9b1b77e';
+    $decodedToken = (object) ['sub' => $resourceOwnerId];
+    $this->assertTrue(CryptrClaimsValidator::validateResourceOwner($decodedToken, $resourceOwnerId));
+  }
+  
+  public function testRightScopes()
+  {
+    $scopes = ['openid', 'email', 'profile'];
+    $decodedToken = (object) ['scp' => $scopes];
+    $this->assertTrue(CryptrClaimsValidator::validateScopes($decodedToken, $scopes));
+  }
+
+  /**
+   * @test
+   */
+  public function testValidRightExpiration()
+  {
+    $mock = $this->createMock(CryptrClaimsValidator::class);
+    $mock->method('currentTime')
+    ->willReturn(new DateTime('@1587027200')); // 2020-04-17 00:00:00
+
+    // 2020-04-17 00:00:01
+    $decodedToken = (object)['exp' => 1587027201];
+    $this->assertTrue($mock->validateExpiration($decodedToken));
+  }
+}
