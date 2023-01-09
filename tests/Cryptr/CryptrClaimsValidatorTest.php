e12bf058c2c0e2aca341347f5bd5e049af7a21e2
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-01-09 11:54:14 +0100

    quick improve and fix cryptrClaimsValidatorTest

--- tests/Cryptr/CryptrClaimsValidatorTest.php
+++ tests/Cryptr/CryptrClaimsValidatorTest.php
@@ -4,13 +4,16 @@ require_once __DIR__ . '../../../vendor/autoload.php';
 
 use Cryptr\CryptrClaimsValidator;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Util\Xml\Validator;
 
+$issuer = 'http://localhost:4000/t/shark-academy';
+$audience = 'http://localhost:3000';
+$resourceOwnerId = '75ee9050-453a-475f-8528-1f92b9b1b77e';
+$scopes = ['openid', 'email', 'profile'];
 class CryptrClaimsValidatorTest extends TestCase
 {
   public function testValidRightIssuer()
   {
-    $issuer = 'http://localhost:4000/t/shark-academy';
+    global $issuer;
     $validator = new CryptrClaimsValidator($issuer, []);
     $decodedToken = (object) ['iss' => $issuer];
     $this->assertTrue($validator->validateIssuer($decodedToken));
@@ -18,7 +21,7 @@ class CryptrClaimsValidatorTest extends TestCase
   
   public function testValidRightAudience()
   {
-    $audience = 'http://localhost:3000';
+    global $audience;
     $validator = new CryptrClaimsValidator("", [$audience]);
     $decodedToken = (object) ['aud' => $audience];
     $this->assertTrue($validator->validateAudience($decodedToken));
@@ -26,14 +29,14 @@ class CryptrClaimsValidatorTest extends TestCase
 
   public function testRightResourceOwner()
   {
-    $resourceOwnerId = '75ee9050-453a-475f-8528-1f92b9b1b77e';
+    global $resourceOwnerId;
     $decodedToken = (object) ['sub' => $resourceOwnerId];
     $this->assertTrue(CryptrClaimsValidator::validateResourceOwner($decodedToken, $resourceOwnerId));
   }
   
   public function testRightScopes()
   {
-    $scopes = ['openid', 'email', 'profile'];
+    global $scopes;
     $decodedToken = (object) ['scp' => $scopes];
     $this->assertTrue(CryptrClaimsValidator::validateScopes($decodedToken, $scopes));
   }
@@ -43,19 +46,8 @@ class CryptrClaimsValidatorTest extends TestCase
    */
   public function testValidRightExpiration()
   {
-    $mock = $this->getMockBuilder(CryptrClaimsValidator::class)
-              ->setMethods(['currentTime'])
-              ->getMock();
-    $mock->expects($this->once())
-          ->method('currentTime')
-          ->will($this->returnValue(new DateTime('@1587027200')));
-    // $mock = $this->createMock(CryptrClaimsValidator::class);
-    // $mock->method('currentTime')
-    // ->willReturn(new DateTime('@1587027200')); // 2020-04-17 00:00:00
-
-    // 2020-04-17 00:00:01
-    $decodedToken = (object)['exp' => 1587027201];
-    $audience = 'http://localhost:3000';
+    global $audience;
+    $decodedToken = (object)['exp' => 1736326441];
     $validator = new CryptrClaimsValidator("", [$audience]);
     $this->assertTrue($validator->validateExpiration($decodedToken));
   }
